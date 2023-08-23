623870cdac7777811b3f827b74058eb626f759e7
NehuenCuenca <nehuen.cuenca@hotmail.com>
2023-08-23 11:07:37 -0300

    mobile desing ready

--- src/App.vue
+++ src/App.vue
@@ -500,4 +500,71 @@ footer {
   color: #A09FB1;
   font: 500 14px 'Raleway', sans-serif;
   place-self: end center;
-}</style>
+}
+
+@media (max-width: 375px) {
+
+  #app .semi-drawer:has(.search-for-places) + .main-content { display: none; }
+
+  .search-buttons {
+    padding: 0 2rem;
+  }
+
+  .parent {
+    overflow: visible;
+  }
+
+  .main-content {
+    gap: 10vh 0;
+  }
+
+  ul.next-days-list {
+    height: max-content;
+    width: 100%;
+    flex-wrap: wrap;
+    gap: 5vh 1vw;
+  }
+
+  li.next-day-item {
+    width: 45%;
+    height: 25vh;
+    justify-content: space-evenly;
+  }
+
+
+
+
+  .todays-hightlights {
+    width: 100%;
+    display: grid;
+    place-self: center center;
+    grid-template-columns: 1fr;
+    grid-template-rows: none;
+    grid-auto-rows: auto;
+    grid-template-areas:
+      "title"
+      "wind "
+      "humidity"
+      "visibility"
+      "air-pressure"
+    ;
+    gap: 5vh 0;
+  }
+
+
+  .todays-hightlights .first-row-wrapper>.hightlight {
+    height: 32vh;
+  }
+
+  .todays-hightlights .hightlight {
+    height: 24vh;
+    gap: 3vh 0;
+  }
+
+  footer {
+    white-space: nowrap;
+  }
+
+
+}
+</style>
